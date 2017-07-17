035dd4f49b82af8679198aa72dc15b254f812d9d
Jason Messer <messer.jr@gmail.com>
2017-07-16 22:19:44 -0700

    added console.table package and implemented minimum order placing functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,25 +5,65 @@ var dbkeys = require('./keys.js');
 
 //console.log('keys:', dbkeys.key);
 
-// var questions = [
-//     {
-//         type:
-//     }
-// ]
+var questions = [
+    {
+        type: 'input',
+        name: 'desired_item',
+        message: 'Please enter the Item ID of the product you wish to purchase.',
+        validate: function(value) {
+            if (parseInt(value) > 0) {
+                return true;
+            }
+            else {
+                return 'Please enter a valid Item ID.'
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'desired_amount',
+        message: 'Please enter the quantity you would like to purchase.',
+        validate: function(value) {
+            if (parseInt(value) > 0) {
+                return true;
+            }
+            else {
+                return 'Please enter a valid quantity.'
+            }
+        }
+    }
+];
 
 var connection = mysql.createConnection(dbkeys.key);
 
 connection.connect();
 
-connection.query('SELECT item_id, product_name, price FROM products', function (error, results, fields) {
+connection.query('SELECT item_id AS \'Item ID\', product_name AS \'Product Name\', price AS \'Unit Price (USD)\' FROM products', function (error, results, fields) {
     if (error) throw error;
 //    console.log('query results are:', results);
     console.table(results);
+    inquirer.prompt(questions).then(function (answers) {
+        connection.query(`SELECT * FROM products WHERE item_id = ${answers.desired_item}`, function (error, results, fields) {
+            if (error) throw error;
+            if (results[0] === undefined ) {
+                console.log('I\'m sorry, but we don\'t have that in stock!');
+            }
+            else if (results[0].stock_quantity < parseInt(answers.desired_amount)) {
+                console.log('I\'m sorry, but we don\'t have that quantity in stock! Please try an order of a lesser quantity.');
+            }
+            else {
+                console.log('Placing your order!');
+                connection.query(`UPDATE products SET stock_quantity = ${results[0].stock_quantity - parseInt(answers.desired_amount)} WHERE item_id = ${answers.desired_item}`, function (error, results, fields) {
+                    if (error) throw error;
+                    console.log('Your order has been placed! Have a nice day!');
+                    connection.end();
+                });
+            }
+        });
+    });
 
 });
 
-// inquirer.prompt([/* Pass your questions in here */]).then(function (answers) {
-//     // Use user feedback for... whatever!! 
-// });
 
-connection.end();
+
+//connection.end();
