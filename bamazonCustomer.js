0d1621ed7db70cf9955d2e1a7563e85cb64599aa
Jason Messer <messer.jr@gmail.com>
2017-07-16 21:34:27 -0700

    reads from database and prints out results

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,29 @@
-var MySQL = require('mysql');
+var mysql = require('mysql');
 var inquirer = require('inquirer');
+require('console.table');
+var dbkeys = require('./keys.js');
+
+//console.log('keys:', dbkeys.key);
+
+// var questions = [
+//     {
+//         type:
+//     }
+// ]
+
+var connection = mysql.createConnection(dbkeys.key);
+
+connection.connect();
+
+connection.query('SELECT item_id, product_name, price FROM products', function (error, results, fields) {
+    if (error) throw error;
+//    console.log('query results are:', results);
+    console.table(results);
+
+});
+
+// inquirer.prompt([/* Pass your questions in here */]).then(function (answers) {
+//     // Use user feedback for... whatever!! 
+// });
+
+connection.end();
